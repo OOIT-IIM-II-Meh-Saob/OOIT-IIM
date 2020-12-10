da50206aba4a8bebb80c3737f4aa7f5bbf0aeec3
Andrej Katin <katin.andrej96@gmail.com>
2020-12-10 12:11:34 +0100

    Vezbe 7

--- OOIT-2020-IM/src/geometry/Test.java
+++ OOIT-2020-IM/src/geometry/Test.java
@@ -1,5 +1,7 @@
 package geometry;
 
+import java.util.Arrays;
+
 public class Test {
 
 	public static void main(String[] args) {
@@ -163,6 +165,143 @@ public class Test {
 		
 		System.out.println("d1 area: " + d1.area());
 		System.out.println("d1 circumference: " + d1.circumference());
+		
+		//VEZBE 7
+		System.out.println("\nVezbe 7:");
+		System.out.println(p1);
+		p1.moveBy(5, 3);
+		System.out.println(p1);
+		p1.moveTo(5, 3);
+		System.out.println(p1);
+		
+		System.out.println(l1);
+		l1.moveBy(5, 3);
+		System.out.println(l1);
+		l1.moveTo(5, 3);
+		System.out.println(l1);
+		
+		System.out.println(r1);
+		r1.moveBy(5, 3);
+		System.out.println(r1);
+		r1.moveTo(5, 3);
+		System.out.println(r1);
+		
+		System.out.println(c1);
+		c1.moveBy(5, 3);
+		System.out.println(c1);
+		c1.moveTo(5, 3);
+		System.out.println(c1);
+		
+		System.out.println(d1);
+		d1.moveBy(5, 3);
+		System.out.println(d1);
+		d1.moveTo(5, 3);
+		System.out.println(d1);
+		
+		Shape d3 = new Donut(p1, 10, 2);
+		Shape p9 = new Point();
+		Shape l9 = l1;
+		Shape c9 = c1;
+		
+		//Shape[] shapes = new Shape[4];
+		Shape[] shapes = { d3, p9, l9, c9 };
+		System.out.println("\n");
+		for (int i = 0; i < shapes.length; i++) {
+			System.out.println(shapes[i]);
+		}
+		
+		for (int i = 0; i < shapes.length; i++) {
+			shapes[i].moveBy(1, 2);
+		}
+		
+		for (int i = 0; i < shapes.length; i++) {
+			System.out.println(shapes[i]);
+		}
+		
+		int[] ints = { 5, 2, 3, 4, 1 };
+		System.out.println("Nesortiran niz: ");
+		for (int i = 0; i < ints.length; i++) {
+			System.out.println(ints[i]);
+		}
+		
+		Arrays.sort(ints);
+		
+		System.out.println("Sortiran niz: ");
+		for (int i = 0; i < ints.length; i++) {
+			System.out.println(ints[i]);
+		}
+		
+		Point p10 = new Point(10, 10);
+		Point p20 = new Point(20, 20);
+		Point p30 = new Point(30, 30);
+		Point p40 = new Point(40, 40);
+		Point[] points = {p30, p10, p20, p40};
+		System.out.println("\nNesortiran niz: ");
+		for (int i = 0; i < points.length; i++) {
+			System.out.println(points[i]);
+		}
+		
+		Arrays.sort(points);
+		
+		System.out.println("Sortiran niz: ");
+		for (int i = 0; i < points.length; i++) {
+			System.out.println(points[i]);
+		}
+		
+		Line l10 = new Line(new Point(10, 10), new Point(100, 100));
+		Line l20 = new Line(new Point(20, 20), new Point(200, 200));
+		Line l30 = new Line(new Point(30, 30), new Point(300, 300));
+		Line l40 = new Line(new Point(40, 40), new Point(400, 400));
+		
+		Line[] lines = {l30, l10, l20, l40};
+		System.out.println("\nNesortiran niz: ");
+		for (int i = 0; i < lines.length; i++) {
+			System.out.println(lines[i]);
+		}
+		
+		Arrays.sort(lines);
+		
+		System.out.println("Sortiran niz: ");
+		for (int i = 0; i < lines.length; i++) {
+			System.out.println(lines[i]);
+		}
+		
+		Rectangle r10 = new Rectangle(p1, 10, 10);
+		Rectangle r20 = new Rectangle(p2, 20, 20);
+		Rectangle r30 = new Rectangle(p3, 30, 30);
+		
+		Rectangle[] rectangles = {r30, r10, r20};
+		System.out.println("\nNesortiran niz: ");
+		for (int i = 0; i < rectangles.length; i++) {
+			System.out.println(rectangles[i] + ", area: " + rectangles[i].area());
+		}
+		
+		Arrays.sort(rectangles);
+		
+		System.out.println("Sortiran niz: ");
+		for (int i = 0; i < rectangles.length; i++) {
+			System.out.println(rectangles[i] + ", area: " + rectangles[i].area());
+		}
+		
+		Circle c10 = new Circle(p1, 10);
+		Circle c20 = new Circle(p1, 20);
+		Circle c30 = new Circle(p1, 30);
+		Donut d40 = new Donut(p1, 30, 10);
+		
+		Circle[] circles = {c30, c10, c20, d40};
+		System.out.println("\nNesortiran niz: ");
+		for (int i = 0; i < circles.length; i++) {
+			System.out.println(circles[i] + ", area: " + circles[i].area());
+		}
+		
+		Arrays.sort(circles);
+		
+		System.out.println("Sortiran niz: ");
+		for (int i = 0; i < circles.length; i++) {
+			System.out.println(circles[i] + ", area: " + circles[i].area());
+		}
+		
+		
 	}
 
 }
